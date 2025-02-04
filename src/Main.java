2d894735fd0725951ab32abaf85571e62eb675e9
Daniel Beachnau <beachndl@mail.uc.edu>
2025-02-03 22:12:37 -0500

    Completed Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Number to examine
+        int numToExamine = 2;
+
+        // Calculate the modulo result
+        int moduloResult = numToExamine % 2;
+
+        // Output the results
+        System.out.println("Modulo result of " + numToExamine + " is " + moduloResult);
+        System.out.println("If the result of mod is 0, it is even.");
+        System.out.println("If the result of mod is 1, it is odd.");
     }
 }
